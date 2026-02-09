80114a1e4daba7277419d7832b7caf4bed142ab1
unknown <malik.tolegen@kaist.ac.kr>
2026-02-09 09:33:12 +0900

    .

--- data-viewer/main.py
+++ data-viewer/main.py
@@ -172,9 +172,7 @@ class DistanceCalculator:
             ToF sample index, or "Not Detected" if not found
         """
 
-        """Uncomment the thing in below to use for sensor from 0109 Data"""
-
-        """
+        
         if len(envelope) <= self.crosstalk_skip + self.min_detection_distance:
             return "Not Detected"
 
@@ -186,9 +184,9 @@ class DistanceCalculator:
         else:
             if len(envelope) < 2000:
                 return "Not Detected"
-            noise_region = envelope[1500:2000]"""
+            noise_region = envelope[1500:2000]
         
-        noise_region = envelope[2500:3500] # THIS IS ONLY FOR SEJINNIM'S SENSOR DATA
+        # noise_region = envelope[2500:3500] # THIS IS ONLY FOR SEJINNIM'S SENSOR DATA
 
         noise_std = np.std(noise_region)
         noise_median = np.median(noise_region)
